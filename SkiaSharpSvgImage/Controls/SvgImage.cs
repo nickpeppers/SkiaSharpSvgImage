01279325fc614c87e0839397e4e0df7def964564
nickpeppers <nicholas.peppers778@topper.wku.edu>
2019-03-14 19:03:00 -0400

    Initial project commit

---
+++ SkiaSharpSvgImage/Controls/SvgImage.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using SkiaSharp.Extended.Svg;
+using SkiaSharp.Views.Forms;
+using Xamarin.Forms;
+
+namespace SkiaSharpSvgImage.Controls
+{
+    public class SvgImage : SKCanvasView
+    {
+        public static readonly BindableProperty SourceProperty = BindableProperty.Create(nameof(Source), typeof(string), typeof(SvgImage), default(string), propertyChanged: OnSourceChanged);
+
+        public string Source
+        {
+            get => (string)GetValue(SourceProperty);
+            set => SetValue(SourceProperty, value);
+        }
+
+        static void OnSourceChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            var skCanvasView = bindable as SKCanvasView;
+            skCanvasView?.InvalidateSurface();
+        }
+
+        protected override void OnBindingContextChanged()
+        {
+            base.OnBindingContextChanged();
+
+            InvalidateSurface();
+        }
+
+        protected override void OnSizeAllocated(double width, double height)
+        {
+            base.OnSizeAllocated(width, height);
+
+            InvalidateSurface();
+        }
+
+        protected override void OnPaintSurface(SKPaintSurfaceEventArgs e)
+        {
+            base.OnPaintSurface(e);
+
+            var surface = e.Surface;
+            var canvas = surface.Canvas;
+
+            canvas.Clear();
+
+            if (string.IsNullOrEmpty(Source))
+                return;
+
+            using (var stream = GetType().Assembly.GetManifestResourceStream("SkiaSharpSvgImage.Resources." + Source))
+            {
+                var skSvg = new SKSvg();
+                skSvg.Load(stream);
+
+                var skImageInfo = e.Info;
+                canvas.Translate(skImageInfo.Width / 2f, skImageInfo.Height / 2f);
+
+                var skRect = skSvg.ViewBox;
+                float xRatio = skImageInfo.Width / skRect.Width;
+                float yRatio = skImageInfo.Height / skRect.Height;
+
+                float ratio = Math.Min(xRatio, yRatio);
+
+                canvas.Scale(ratio);
+                canvas.Translate(-skRect.MidX, -skRect.MidY);
+
+                canvas.DrawPicture(skSvg.Picture);
+            }
+        }
+    }
+}
